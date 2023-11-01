ceda90aafd5ed4c9473417403b73c70f9a0c61f9
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-11-01 20:08:24 +0100

    create all neccessary methods in UserRepository

--- src/main/java/com/app/repository/UserRepository.java
+++ src/main/java/com/app/repository/UserRepository.java
@@ -1,5 +1,16 @@
 package com.app.repository;
 
-public interface UserRepository {
+import org.springframework.data.repository.CrudRepository;
 
+public interface UserRepository extends CrudRepository<User, Long>  {
+
+    User saveUser(User user);
+    
+    Optional<User> findUserById(Long id);
+    
+    List<User> getAllUsers();
+    
+    User updateUser(User user);
+    
+    void deleteUser(Long id);
 }

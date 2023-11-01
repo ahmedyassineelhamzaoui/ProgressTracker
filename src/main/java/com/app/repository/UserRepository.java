cd1b21ed8eabf102a12373e722accd71e1b3994f
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-11-01 20:14:57 +0100

    import all neccessary classes in UserRepository

--- src/main/java/com/app/repository/UserRepository.java
+++ src/main/java/com/app/repository/UserRepository.java
@@ -1,6 +1,10 @@
 package com.app.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import com.app.model.User;
+import java.util.Optional;
+import java.util.List;
+
 
 public interface UserRepository extends CrudRepository<User, Long>  {
 

a4129414404f5ca31bf8e392599563302f37ec70
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-11-02 08:56:47 +0100

    add all necessary feilds in User entity

--- src/main/java/com/app/model/User.java
+++ src/main/java/com/app/model/User.java
@@ -1,7 +1,13 @@
+import java.util.List;
+
+import com.app.entity.Task;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class User {
@@ -10,16 +16,27 @@ public class User {
     private Long id;
     
     private String name;
+    @Column(unique = true)
     private String email;
-    
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    @OneToMany(mappedBy = "assignedUser")
+    private List<Task> assignedTasks;
     
     public User() {
       super();
     }
     
-    public User(String name, String email) {
+    public User(String name, String email,String username,String lastName,String password,String firstName) {
         this.name = name;
         this.email = email;
+        this.username = username;
+        this.password = password;
+        this.lastName = lastName;
+        this.firstName = firstName;
     }
     
     
@@ -46,4 +63,43 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public List<Task> getAssignedTasks() {
+		return assignedTasks;
+	}
+
+	public void setAssignedTasks(List<Task> assignedTasks) {
+		this.assignedTasks = assignedTasks;
+	}
 }

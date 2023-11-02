3d24c0069ff3764f08ad9ecca5a950196145674e
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-11-02 09:00:17 +0100

    create the task entity

---
+++ src/main/java/com/app/model/Task.java
@@ -0,0 +1,66 @@
+package com.app.model;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Date deadline;
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User assignedUser;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(Date deadline) {
+		this.deadline = deadline;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public User getAssignedUser() {
+		return assignedUser;
+	}
+
+	public void setAssignedUser(User assignedUser) {
+		this.assignedUser = assignedUser;
+	}
+
+	@Override
+	public String toString() {
+		return "Task [id=" + id + ", description=" + description + ", deadline=" + deadline
+				+ ", status=" + status + ", assignedUser=" + assignedUser + "]";
+	}
+    
+
+}

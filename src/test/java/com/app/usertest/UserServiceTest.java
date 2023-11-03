47a7c8325d30a852bd29e082355f16b262b549af
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-11-03 09:34:16 +0100

    create the class saveUserTest

---
+++ src/test/java/com/app/usertest/UserServiceTest.java
@@ -0,0 +1,24 @@
+package com.app.usertest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserServiceTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSaveUser() {
+		
+	}
+
+}

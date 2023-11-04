b25f149e369861134ecd2e74167653aed48bbbfb
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-11-04 14:49:03 +0100

    create the method to add user in database

--- src/main/java/com/app/repository/Test.java
+++ src/main/java/com/app/repository/Test.java
@@ -1,8 +1,10 @@
 package com.app.repository;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
+import com.app.exceptions.DuplicateUserException;
 import com.app.model.User;
 import com.app.service.UserService;
 
@@ -17,17 +19,17 @@ public class Test {
 	    user.setLastName("yassine");
 	    user.setFirstName("ahmed");
 	    user.setPassword("eRROR404@");
-	    User savedUser ;
+	    User savedUser = null ;
 	    try {
-		     savedUser = userService.saveUser(user);
-	    } catch (DuplicateUserException e) {
-	        // Handle the exception, such as displaying an error message to the user
-	        System.out.println("Error: " + e.getMessage());
+	        savedUser = userService.saveUser(user);
+	        if(savedUser != null) {
+		        System.out.println("User saved successfully: " + savedUser);
+		    }else {
+		    	System.out.println("no there is an error");
+		    }
+	    } catch (DataIntegrityViolationException e) {
+	        System.out.println("Error: Duplicate user found with email " + user.getEmail());
 	    }
-	    if(savedUser != null) {
-	        System.out.println("User saved successfully: " + savedUser);
-	    }else {
-	    	System.out.println("no there is an error");
-	    }
+	    
     }
 }
